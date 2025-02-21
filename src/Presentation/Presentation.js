a84770c5637366a4ac2b0f78d3a4e663f006686e
PierrePivz <chillingd@macbook-pro-de-pierre.home>
2025-02-21 19:04:37 +0100

    new

--- src/Presentation/Presentation.js
+++ src/Presentation/Presentation.js
@@ -19,8 +19,8 @@ return(
 <p>Au programme : repos, détoxification du corps, méditation, nourriture revitalisante,<br/> eau de source, sauna et ballades dans la région.
   Pour apprendre en intelligence de nouvelles habitudes de vie.</p></div>
 <div class="row">
- <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSVMFRIclqHcwFmbRbiNax95UEconCpTd3ZRvXOkARFplPSw/viewform"><button class="button_rdv" id="button_retraite">Je suis intérressé.e</button></a>
- <a href="https://www.dropbox.com/scl/fi/51zk5fypc4lgor58h66br/Retraite-Yoga-Ayurv-da-Dr-me-Avril-2025-NC.pdf?rlkey=hmhiwfhgb1wraq4ex3px90uxc&e=1&st=q4h6egov&dl=0"><button class="button_rdv" id="button_retraite">En savoir plus</button></a></div>
+ <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSVMFRIclqHcwFmbRbiNax95UEconCpTd3ZRvXOkARFplPSw/viewform" target="_blank"><button class="button_rdv" id="button_retraite">Je suis intérressé.e</button></a>
+ <a href="https://www.dropbox.com/scl/fi/51zk5fypc4lgor58h66br/Retraite-Yoga-Ayurv-da-Dr-me-Avril-2025-NC.pdf?rlkey=hmhiwfhgb1wraq4ex3px90uxc&e=1&st=q4h6egov&dl=0" target="_blank"><button class="button_rdv" id="button_retraite">En savoir plus</button></a></div>
  </div>    
 
     
