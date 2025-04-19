1235eed38bcc099365e03c392d23c63f6c449593
PierrePivz <chillingd@macbook-pro-de-pierre.home>
2025-04-19 16:49:51 +0200

    new

--- src/App.js
+++ src/App.js
@@ -9,8 +9,20 @@ import Parcours from "./Parcours/Parcours.js";
 import Temoignages from "./Temoignages/Temoignages.js";
 import Footer from "./Footer/Footer.js";
 import Retraite from "./Retraite/Retraite.js";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+
+    
+    window.location.href = "https://www.medoucine.com/consultation/paris/victor-clement/2272";
+
+    
+  });
+
+  
+ 
   return (
     <div className="App">
 
