5e217d2f54903ac5d99004aac04492246973f236
Pierre Pivin <pierrepivin44@gmail.com>
2025-11-09 18:14:55 +0100

    new

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,9 +5,7 @@ import medoucine from "../images/medoucine.svg";
 
 export default function Temoignages(){
 
- function redirect(){
-     window.location = "https://www.medoucine.com/consultation/paris/victor-clement/2272?fbclid=IwY2xjawF-CO1leHRuA2FlbQIxMAABHU-NVR8rWbF1u26FzjfBlgO7QdtKHi4DjgDHFf2tafz4gbfsKSuNJyEGUw_aem_7D5NEyztYc6CzZ7vDh2c_w";
- }   
+ 
 
 return(
 
@@ -16,7 +14,7 @@ return(
 
     <div class="Footer block">
 
-        <button class="button_rdv" onClick={redirect}>Prendre rendez-vous</button>
+        <button class="button_rdv" ><a target="#" href="https://www.medoucine.com/consultation/paris/victor-clement/2272?fbclid=IwY2xjawF-CO1leHRuA2FlbQIxMAABHU-NVR8rWbF1u26FzjfBlgO7QdtKHi4DjgDHFf2tafz4gbfsKSuNJyEGUw_aem_7D5NEyztYc6CzZ7vDh2c_w">Prendre rendez-vous</a></button>
 
         <p>Victor Clément Ayurvéda<br/><br/>
 victorclementayu@protonmail.com<br/><br/>
@@ -24,9 +22,9 @@ victorclementayu@protonmail.com<br/><br/>
 06 23 41 54 76</p>
 
         <div class="links">
-        <a href="https://www.facebook.com/profile.php?id=100075737362989"><img src={facebook} ></img></a>
-        <a href="https://www.instagram.com/clement_ayurveda/"><img src={insta}></img></a>
-        <a href="https://www.medoucine.com/consultation/paris/victor-clement/2272?fbclid=IwY2xjawF-CO1leHRuA2FlbQIxMAABHU-NVR8rWbF1u26FzjfBlgO7QdtKHi4DjgDHFf2tafz4gbfsKSuNJyEGUw_aem_7D5NEyztYc6CzZ7vDh2c_w"><img src={medoucine}></img></a></div>
+        <a href="https://www.facebook.com/profile.php?id=100075737362989" target="#"><img src={facebook} ></img></a>
+        <a href="https://www.instagram.com/clement_ayurveda/" target="#"><img src={insta}></img></a>
+        <a href="https://www.medoucine.com/consultation/paris/victor-clement/2272?fbclid=IwY2xjawF-CO1leHRuA2FlbQIxMAABHU-NVR8rWbF1u26FzjfBlgO7QdtKHi4DjgDHFf2tafz4gbfsKSuNJyEGUw_aem_7D5NEyztYc6CzZ7vDh2c_w" target="#"><img src={medoucine}></img></a></div>
         
         
 
